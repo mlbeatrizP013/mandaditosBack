5fa42eac8b108e5bee45935808d3b205af3d765d
Beatriz Patricia Martínez López <mlbeatrice013@gmail.com>
2025-07-24 18:44:07 -0600

    archivos para cliente,direccion,estado Archivos modificados de module.ts,controllers.ts,service.ts, los Dtos de create y update de los archivos Cliente,Direccion y Estatus

--- src/direccion/dto/update-direccion.dto.ts
+++ src/direccion/dto/update-direccion.dto.ts
@@ -1,4 +1,37 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDireccionDto } from './create-direccion.dto';
+import { IsString, IsNumber, IsOptional } from 'class-validator';
 
-export class UpdateDireccionDto extends PartialType(CreateDireccionDto) {}
+export class UpdateDireccionDto {
+    @IsString()
+    @IsOptional()
+    calle?: string;
+
+    @IsNumber()
+    @IsOptional()
+    num_casa?: number;
+
+    @IsNumber()
+    @IsOptional()
+    CP?: number;
+
+    @IsString()
+    @IsOptional()
+    colonia?: string;
+
+    @IsString()
+    @IsOptional()
+    municipio?: string;
+
+    @IsString()
+    @IsOptional()
+    estado?: string;
+
+    @IsString()
+    @IsOptional()
+    alias?: string;
+
+    @IsString()
+    @IsOptional()
+    referencia?: string;
+}
