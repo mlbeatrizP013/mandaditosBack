206e74166e06e5246a6fb398594b8c7dc49785e7
Jose <utti222001@utvco.edu.mx>
2025-07-31 08:52:11 -0600

    creado DTO, create pedido

--- src/pedido/dto/create-pedido.dto.ts
+++ src/pedido/dto/create-pedido.dto.ts
@@ -1,1 +1,46 @@
-export class CreatePedidoDto {}
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString, IsDate, IsArray } from 'class-validator';
+
+export class CreatePedidoDto {
+  @IsNotEmpty()
+  @IsString()
+  categoria: string;
+
+  @IsNotEmpty()
+  lugar_entrega: number;
+
+  @IsNotEmpty()
+  lugar_recoleccion: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  precio: number;
+
+  @IsEnum(['En espera', 'Aceptado', 'En camino', 'Entregado'])
+  estatus: string;
+
+  @IsOptional()
+  @IsString()
+  notas?: string;
+
+  @IsOptional()
+  @IsDate()
+  fecha?: Date;
+
+  @IsNotEmpty()
+  @IsDate()
+  fecha_entrega_estimada: Date;
+
+  @IsOptional()
+  @IsDate()
+  fecha_entrega_real?: Date;
+
+  @IsNotEmpty()
+  cliente: number; 
+
+  @IsNotEmpty()
+  repartidor: number;
+
+  @IsOptional()
+  @IsArray()
+  historial?: number[];
+}    
